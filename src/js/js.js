8f0b3d3b9b4a4b6e36bdf1761282b916b87cb2c4
Eugene Farafonov <eu.farafonov@gmail.com>
2017-09-24 22:25:05 +0300

    drugi part 2

--- src/js/js.js
+++ src/js/js.js
@@ -1,15 +1,63 @@
-var allFriends;
+var allFriends = [];
+var selectFriends = [];
 var filterContainer = document.getElementById('filter_container');
 var filterLeft = document.getElementById('js_filter_left');
 var filterRight = document.getElementById('js_filter_right');
 var friendListLeft = document.getElementById('js_friend_list_left');
 var friendListRight = document.getElementById('js_friend_list_right');
-
-filterRight.setAttribute('disabled', '');
+var filterSave = document.querySelector('.filter_save');
 
 filterLeft.value = '';
 filterRight.value = '';
 
+if (localStorage.left && localStorage.right) {
+    console.log(2);
+    allFriends = JSON.parse(localStorage.left);
+    selectFriends = JSON.parse(localStorage.right);
+    
+    friendListLeft.innerHTML = '';
+    friendListRight.innerHTML = '';
+    
+    allFriends.forEach(function(friend){
+        showFriend(friend, friendListLeft);
+    });
+    
+    selectFriends.forEach(function(friend){
+        showFriend(friend, friendListRight);
+    });
+} else {
+    console.log(1);
+    const promise = new Promise(function(resolve, reject){
+        VK.init( {apiId: 6194358} );
+        
+        VK.Auth.login(function(data){
+            if (data.session) {
+                resolve(data);
+            } else {
+                reject(new Error('Ошибка авторизации!'));
+            }
+        }, 8);
+    });
+
+    promise
+        .then(() => {
+            return api('friends.get', { v: 5.68, fields: 'first_name, last_name, photo_100', order: 'name' })
+        })
+        .then(data => {
+            allFriends = data.items;
+            
+            friendListLeft.innerHTML = '';
+            
+            allFriends.forEach(function(friend){
+                showFriend(friend, friendListLeft);
+            });
+        })
+        .catch(function (e) {
+            alert('Ошибка: ' + e.message);
+        });
+}
+
+/* Первоначальное заполнение списка друзей */
 function api(method, params) {
     return new Promise((resolve, reject) => {
         VK.api(method, params, data => {
@@ -22,39 +70,12 @@ function api(method, params) {
     });
 }
 
-const promise = new Promise(function(resolve, reject){
-    VK.init( {apiId: 6194358} );
-    
-    VK.Auth.login(function(data){
-        if (data.session) {
-            resolve(data);
-        } else {
-            reject(new Error('Ошибка авторизации!'));
-        }
-    }, 8);
-});
-
-promise
-    .then(() => {
-        return api('friends.get', { v: 5.68, fields: 'first_name, last_name, photo_100', order: 'name' })
-    })
-    .then(data => {
-        allFriends = data.items;
-        friendListLeft.innerHTML = '';
-        
-        allFriends.forEach(function(friend){
-            showFriend(friend, friendListLeft);
-        });
-    })
-    .catch(function (e) {
-        alert('Ошибка: ' + e.message);
-    });
-
 function showFriend(friend, parentBlock) {
     let friendLi = document.createElement('li');
     friendLi.classList.add('filter_friend_item');
     friendLi.classList.add('js_drag');
     friendLi.classList.add('clearfix');
+    friendLi.dataset.id = friend.id;
     
     let friendImg = document.createElement('img');
     friendImg.classList.add('filter_friend_img');
@@ -73,21 +94,23 @@ function showFriend(friend, parentBlock) {
     friendLi.appendChild(friendBtn);
     
     parentBlock.appendChild(friendLi);
-    //addDND(friendLi);
 }
 
-filterContainer.addEventListener('keyup', function(event){
+/* Фильтрация списков */
+document.addEventListener('keyup', function(event){
     let target = event.target;
     
     if (target.nodeName !== 'INPUT') return false;
     
-    let filterText = event.target.value;
-    let parentId = 'js_friend_list_' + target.id.split('_')[2];
+    let filterText = target.value;
+    let side = target.id.split('_')[2];
+    let parentId = 'js_friend_list_' + side;
     let parentEl = document.getElementById(parentId);
+    let arr = (side === 'left') ? allFriends : selectFriends;
     
     parentEl.innerHTML = '';
     
-    allFriends.forEach(function(friend){
+    arr.forEach(function(friend){
         if ( filterFriend(friend.first_name, filterText) || filterFriend(friend.last_name, filterText) ) {
             showFriend(friend, parentEl);
         }
@@ -98,112 +121,171 @@ function filterFriend(name, filter) {
     return (name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
 }
 
-//////////////////////////////////////////
+/* Сохранение списков */
+filterSave.addEventListener('click', function(event){
+    localStorage.left = JSON.stringify(allFriends);
+    localStorage.right = JSON.stringify(selectFriends);
+});
+
+/* Перемещение друзей по клику */
+filterContainer.addEventListener('click', function(event){
+    var target = event.target;
+    
+    if (target.classList.contains('action')) {
+        var moveEl = target.parentElement;
+        var moveElId = moveEl.dataset.id;
+        var side = moveEl.parentElement.id.split('_')[3];
+        var arrId = null;
+        
+        function changeElement(arr1, arr2) {
+            for (var i = 0; i < arr1.length; i++) {
+                if (arr1[i].id == moveElId){
+                    arrId = i;
+                    break;
+                }
+            }
+            
+            if (arrId !== null) {
+                var delEl = arr1.splice(arrId, 1);
+                arr2.push(delEl[0]);
+            }
+        }
+        
+        if (side === 'left') {
+            changeElement(allFriends, selectFriends);
+        } else {
+            changeElement(selectFriends, allFriends);
+        }
+        
+        friendListLeft.innerHTML = '';
+        friendListRight.innerHTML = '';
+        
+        allFriends.forEach(function(friend){
+            showFriend(friend, friendListLeft);
+        });
+        
+        selectFriends.forEach(function(friend){
+            showFriend(friend, friendListRight);
+        });
+    }
+});
+
+/* Перемещение друзей DnD */
 var dragObject = {};
 
-filterContainer.addEventListener('mousedown', function(event){
-    if (e.which !== 1) return;
+document.addEventListener('mousedown', function(event){
+    if (event.which !== 1) return  false;
     
     var elem = event.target.closest('.js_drag');
     
-    if (!elem) return;
+    if (!elem) return false;
     
     dragObject.elem = elem;
     dragObject.downX = event.pageX;
     dragObject.downY = event.pageY;
 });
 
-filterContainer.addEventListener('mousemove', function(event){
-    if (!dragObject.elem) return;
+document.addEventListener('mousemove', function(event){
+    if (!dragObject.elem) return false;
     
     if (!dragObject.avatar) {
         var moveX = event.pageX - dragObject.downX;
         var moveY = event.pageY - dragObject.downY;
         
-        if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) return;
+        if (Math.abs(moveX) < 10 && Math.abs(moveY) < 10) return false;
         
         dragObject.avatar = createAvatar(event);
         
         if (!dragObject.avatar) {
-            dragObject = {}; // аватар создать не удалось, отмена переноса
-            return; // возможно, нельзя захватить за эту часть элемента
+            dragObject = {};
+            return;
         }
         
-        // аватар создан успешно
-        // создать вспомогательные свойства shiftX/shiftY
         var coords = getCoords(dragObject.avatar);
+        
         dragObject.shiftX = dragObject.downX - coords.left;
         dragObject.shiftY = dragObject.downY - coords.top;
         
-        startDrag(e); // отобразить начало переноса
+        startDrag(event);
     }
     
-    // отобразить перенос объекта при каждом движении мыши
-    dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
-    dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+    dragObject.avatar.style.left = event.pageX - dragObject.shiftX + 'px';
+    dragObject.avatar.style.top = event.pageY - dragObject.shiftY + 'px';
     
     return false;
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function addDND(target) {
-    target.addEventListener('mousedown', function(event) {
-        var shiftX = event.pageX - getCoords(target).left;
-        var shiftY = event.pageY - getCoords(target).top;
-        
-        
-        elW = target.clientWidth;
-        //console.log(elW);
-        
-        target.style.width = elW + 'px';
-        target.classList.add('filter_friend_item_move');
-        
-        document.addEventListener('mousemove', mMove);
-        
-        function mMove(event) {
-            target.style.left = event.pageX - shiftX + 'px';
-            target.style.top = event.pageY - shiftY + 'px';
-        }
-        
-        target.addEventListener('mouseup', mUp);
-        
-        function mUp() {
-            document.removeEventListener('mousemove', mMove);
-            target.removeEventListener('mouseup', mUp);
-            target.classList.remove('filter_friend_item_move');
-        }
-        
-        function getCoords(target) {
-            var box = target.getBoundingClientRect();
-
-            return {
-                top: box.top + pageYOffset,
-                left: box.left + pageXOffset
-            };
-        
-        }
-    });
+function createAvatar(e) {
+    var avatar = dragObject.elem;
+    var old = {
+        parent: avatar.parentElement,
+        nextSibling: avatar.nextElementSibling,
+        position: avatar.position || '',
+        left: avatar.left || '',
+        top: avatar.top || '',
+        zIndex: avatar.zIndex || ''
+    };
+    
+    avatar.rollback = function() {
+        old.parent.insertBefore(avatar, old.nextSibling);
+        avatar.style.position = old.position;
+        avatar.style.left = old.left;
+        avatar.style.top = old.top;
+        avatar.style.zIndex = old.zIndex
+    };
+    
+    return avatar;
 }
 
-*/
+function getCoords(target) {
+    var box = target.getBoundingClientRect();
 
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+}
 
+function startDrag(event) {
+    var avatar = dragObject.avatar;
+    
+    document.body.appendChild(avatar);
+    avatar.style.zIndex = 1000;
+    avatar.style.position = 'absolute';
+}
 
+document.addEventListener('mouseup', function(event){
+    if (dragObject.avatar) {
+        finishDrag(event);
+    }
+    
+    dragObject = {};
+});
+
+function finishDrag(e) {
+    var dropElem = findDroppable(e);
+    
+    if (dropElem) {
+        //dropElem.appendChild(dragObject.avatar);
+        //dragObject.avatar.style.zIndex = '';
+        //dragObject.avatar.style.position = '';
+        //console.log(dragObject.avatar);
+        console.log('OK');
+    } else {
+        console.log('ERROR');
+    }
+}
+
+function findDroppable(event) {
+    dragObject.avatar.hidden = true;
+    
+    var elem = document.elementFromPoint(event.clientX, event.clientY);
+    
+    dragObject.avatar.hidden = false;
+    
+    if (elem == null) {// такое возможно, если курсор мыши "вылетел" за границу окна
+        return null;
+    }
+    
+    return elem.closest('.js_drop');
+}
